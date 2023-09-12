725d21de5be5ac4785e6024c913af250f55e36b7
Sam Gordon <sam.gordon@csiro.au>
2023-09-12 15:03:53 +1000

    added tests for make_stars function

--- tests/test_sky_sim.py
+++ tests/test_sky_sim.py
@@ -1,6 +1,8 @@
 import pytest
+import numpy as np
 
-from mymodule.sky_sim import get_radec, segadecimal_to_float, float_to_segadecimal
+from mymodule.sky_sim import get_radec, make_stars
+# segadecimal_to_float, float_to_segadecimal
 
 def test_module_import():
     try:
@@ -17,10 +19,33 @@ def test_get_radec_values():
     assert ra == pytest.approx(14.215420962967535, rel = 1e-20)
     assert dec == pytest.approx(41.26916666666667, rel = 1e-20)
 
-@pytest.mark.parameterize("ra,dec", [(14.215420962967535,41.26916666666667)])
-def test_segadecimal_to_float(ra, dec):
-    new_ra, new_dec = segadecimal_to_float(
-        *float_to_segadecimal(ra, dec))
+@pytest.mark.parametrize("ra,dec,nsrc", [(0, 0, 100), (0, 0, 1000)])
+def test_make_stars_length(ra, dec, nsrc):
+    """
+    Check if the array output by make_stars is the right length
+    """
+    ras, decs = make_stars(ra, dec, nsrc=nsrc)
+
+    assert len(ras) == nsrc
+    assert len(decs) == nsrc
+
+@pytest.mark.parametrize("ra,dec", [(0,0), (14.215420962967535,41.26916666666667)])
+def test_make_stars_range(ra, dec):
+    """
+    Check all of the values generated are in the right range (within one degree of the centre)
+    """
+    ras, decs = make_stars(ra, dec)
+
+    assert np.any(np.array(ras) > ra - 1)
+    assert np.any(np.array(ras) < ra + 1)
+
+    assert np.any(np.array(decs) > dec - 1)
+    assert np.any(np.array(decs) < dec + 1)
+
+# @pytest.mark.parameterize("ra,dec", [(14.215420962967535,41.26916666666667)])
+# def test_segadecimal_to_float(ra, dec):
+#     new_ra, new_dec = segadecimal_to_float(
+#         *float_to_segadecimal(ra, dec))
     
-    assert new_ra == pytest.approx(ra)
-    assert new_dec == pytest.approx(dec)
+#     assert new_ra == pytest.approx(ra)
+#     assert new_dec == pytest.approx(dec)
